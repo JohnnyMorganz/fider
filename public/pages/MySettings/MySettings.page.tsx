8b92071bdaf58cc80026df75c861c08c2d43c195
JohnnyMorganz <theROBLOXBot@gmail.com>
2020-05-15 16:34:39 +0100

    Fix

--- public/pages/MySettings/MySettings.page.tsx
+++ public/pages/MySettings/MySettings.page.tsx
@@ -41,7 +41,7 @@ export default class MySettingsPage extends React.Component<MySettingsPageProps,
 
   private confirm = async () => {
     const result = await actions.updateUserSettings({
-      name: this.state.name,
+      //name: this.state.name,
       avatarType: this.state.avatarType,
       avatar: this.state.avatar,
       settings: this.state.userSettings
@@ -105,11 +105,11 @@ export default class MySettingsPage extends React.Component<MySettingsPageProps,
   };
 
   public render() {
-    const changeEmail = (
+    /*const changeEmail = (
       <span className="ui info clickable" onClick={this.startChangeEmail}>
         change
       </span>
-    );
+    );*/
 
     return (
       <div id="p-my-settings" className="page container">
